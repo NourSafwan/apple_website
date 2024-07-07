a1a9893b5dca1ffbd6df8066873d3df94534d35b
Nour Safwan <nouraldin374@gmail.com>
2024-07-07 21:27:38 +0300

    chore: Update Footer component to include flex class for footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
           <p className="font-semibold text-gray text-xs">Copyright @ 2024 Apple Inc. All rights reserved.</p>
           <div className="flex">
             {footerLinks.map((link, i) => (
-              <p key={link} className="font-semibold text-gray text-xs">
+              <p key={link} className="font-semibold text-gray text-xs flex">
                 {link}{' '}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
