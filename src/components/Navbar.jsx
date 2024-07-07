1bc69c51094c34fa9352009abec8372314d1d038
Nour Safwan <nouraldin374@gmail.com>
2024-07-07 22:51:44 +0300

    adding mobile nav styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { appleImg, bagImg, searchImg } from "../utils";
-import { navLists } from './../constants';
+import { navLists } from "./../constants";
 
+import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
+
+  const  [nav, setNav] = useState(false)
+
+  console.log(nav);
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width justify-center items-center">
         <img src={appleImg} alt="apple" width={14} height={18} />
-        <div className="flex flex-1 justify-center items-center max-sm:hidden">
-          {navLists.map(
-            (item) => (
-              <div key={item} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">{item}</div>
-            )
-          )}
+        <div className={`flex flex-1 justify-center items-center  ${nav ? "mobileNav" : "max-sm:hidden"}`}>
+          {navLists.map((item) => (
+            <div
+              key={item}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {item}
+            </div>
+          ))}
         </div>
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <GiHamburgerMenu alt="search" width={18} height={18} className="max-sm:block hidden" onClick={() => setNav(!nav)}/>
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
