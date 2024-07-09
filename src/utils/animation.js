fafbbacc08c5219b5ac891d8bb34e2374943727b
Nour Safwan <nouraldin374@gmail.com>
2024-07-09 08:20:12 +0300

    chore: Adjust scroll trigger start position in animateWithGsap function

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
+
 export const animateWithGsap = (target, animationProps, scrollProps) => {
   gsap.to(target, {
     ...animationProps,
     scrollTrigger: {
       trigger: target,
       toggleActions: "restart reverse restart reverse",
-      start: "10% bottom",
+      start: "top 85%",
       ...scrollProps,
     },
   });
